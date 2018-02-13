6bec8549c39faaa2a7efc0ae06e31dc49ea563e7
amrm2018 <amr30848@gmail.com>
2018-02-14 01:38:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/amr1/atf_mysql_1/Create_Post_Activity.java
@@ -0,0 +1,13 @@
+package com.example.amr1.atf_mysql_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Create_Post_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create__post);
+    }
+}
---
+++ app/src/main/java/com/example/amr1/atf_mysql_1/Login_Activity.java
@@ -0,0 +1,16 @@
+package com.example.amr1.atf_mysql_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+    }
+}
