492cc4fb677cdc7be4c57474ca87ae0f62e48c09
amrm2018 <amr30848@gmail.com>
2018-02-16 02:19:12 +0200

    update 3

--- app/src/main/java/com/example/amr1/atf_mysql_1/Register_Activity.java
+++ app/src/main/java/com/example/amr1/atf_mysql_1/Register_Activity.java
@@ -22,7 +22,7 @@ import java.sql.Statement;
 
 public class Register_Activity extends AppCompatActivity {
 
-    EditText txtfname , txtemail ,  txtphone  ;
+    EditText txtfname , txtemail ,  txtphone  ,txtpass ,txtcomfpass ;
     RadioButton rdmale , rdfemale;
     Button btn_save_reg;
 
@@ -32,51 +32,20 @@ public class Register_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        txtfname=(EditText) findViewById(R.id.ed_fullname_reg);
-
-        final EditText txtemail=(EditText) findViewById(R.id.ed_email_reg);
-        final EditText  txtpass=(EditText) findViewById(R.id.ed_pass);
-        final EditText txtcomfpass=(EditText) findViewById(R.id.ed_comfpass_reg);
-
-        txtphone=(EditText) findViewById(R.id.ed_phone_reg);
-
-
-        rdmale=(RadioButton)findViewById(R.id.rdo_male);
-        rdfemale=(RadioButton)findViewById(R.id.rdo_female);
-        btn_save_reg=(Button)findViewById(R.id.btn_register);
+        txtfname=findViewById(R.id.ed_fullname_reg);
+        txtemail=findViewById(R.id.ed_email_reg);
+        txtpass=findViewById(R.id.ed_pass);
+        txtcomfpass=findViewById(R.id.ed_comfpass_reg);
+        txtphone=findViewById(R.id.ed_phone_reg);
+        rdmale=findViewById(R.id.rdo_male);
+        rdfemale=findViewById(R.id.rdo_female);
+        btn_save_reg=findViewById(R.id.btn_register);
 
         btn_save_reg.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                //==================  star code insert
-                DB_Connection db = new DB_Connection();
-                Connection conn = db.connectionclass();
-                if (conn == null) {
-                    Toast.makeText(Register_Activity.this, " Check Internet", Toast.LENGTH_LONG).show();
-                }
-                else {
-                    try {
-                        Statement stmt = conn.createStatement();
-                        String gen;
-                        if (rdmale.isChecked())
-                            gen = "male";
-                        else
-                            gen = "female";
-                        int z = stmt.executeUpdate("insert into t_user (full_name,email,password,phone,gender) Values('" + txtfname.getText() + "','" + txtemail.getText() + "','" + txtpass.getText() + "','" + gen + "','" + txtphone.getText() + "')");
-                        if (z >= 1) {
-                            startActivity(new Intent(Register_Activity.this , Home_Activity.class));
-                            Toast.makeText(Register_Activity.this, " Saved Register  ", Toast.LENGTH_SHORT).show();
-                        }
-                        else {
 
-                            Toast.makeText(Register_Activity.this, " Not Saved Register", Toast.LENGTH_LONG).show();
-                        }
-                    } catch (SQLException e) {
-                        Toast.makeText(Register_Activity.this, " Error is:   " + e.getMessage(), Toast.LENGTH_LONG).show();
-
-                    }
-                }//========end code insert
 
             }
         });//--------------end btn_save
